e77797544aa5be45a639e00d8d531bc244c2285c
Epicmanpreet02 <144267040+Epicmanpreet01@users.noreply.github.com>
2025-03-19 23:32:36 +0530

    made the cart using a class or an object generator instead of making an object directly, giving more features like a constructor and ability to create objects with same blueprint

--- backend/checkout.js
+++ backend/checkout.js
@@ -2,6 +2,7 @@ import loadCart from "./checkout/cartSummary.js";
 import loadHeader from './checkout/head.js';
 import loadOrderSummary from "./checkout/orderSummary.js";
 // import cart from '../data/cart-oop.js';
+// import {cart, businessCart} from '../data/cart-class.js';
 
 export default function checkOutLoader() {
     loadCart();
