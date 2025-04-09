03d84643c90af68b4523f3d5f3da8730eda43517
Epicmanpreet02 <144267040+Epicmanpreet01@users.noreply.github.com>
2025-04-09 19:20:10 +0530

    clean up

--- backend/checkout.js
+++ backend/checkout.js
@@ -2,10 +2,6 @@ import {cart as cartObj} from '../data/cart.js';
 import loadCart from "./checkout/cartSummary.js";
 import loadHeader from './checkout/head.js';
 import loadOrderSummary from "./checkout/orderSummary.js";
-// import '../data/car.js';
-// import cart from '../data/cart-oop.js';
-// import {cart, businessCart} from '../data/cart-class.js';
-import './backend.js';
 import { loadProducts } from '../data/products.js';
 
 
